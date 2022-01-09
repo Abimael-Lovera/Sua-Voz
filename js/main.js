02f6e9e196fc358daf2ef418a3348752fbf7eaaa
Abimael Lovera <abimaellovera@gmail.com>
2022-01-09 01:41:08 -0300

    Images Team Home About-Hero Rights

--- js/main.js
+++ js/main.js
@@ -4,10 +4,10 @@ counters.forEach(counter => {
 	counter.innerText = '0';
 	const updateCounter = () => {
 		const target = +counter.getAttribute('data-target');
-		console.log(typeof target, target);
+		// console.log(typeof target, target);
 		const c = +counter.innerText;
 		const increment = target / 100;
-		console.log(increment);
+		// console.log(increment);
 
 		if (c < target) {
 			counter.innerText = `${Math.ceil(c + increment)}`;
